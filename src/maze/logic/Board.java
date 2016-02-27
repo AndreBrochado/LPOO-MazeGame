a3a526efb9f9a33719d0644abcf2819b845d1756
AndreBrochado <andrebrreis@gmail.com>
2016-02-27 15:11:59 +0000

    Add Board fields and constructors

--- src/maze/logic/Board.java
+++ src/maze/logic/Board.java
@@ -4,4 +4,23 @@ package maze.logic;
  * Created by Andre on 27/02/2016.
  */
 public class Board {
+
+    private int height, width;
+    private GameObject[][] board;
+    private GameObject[] objects;
+    private GameCharacter[] characters;
+
+    public Board(int height, int width){
+        this.height = height;
+        this.width = width;
+        this.board = new GameObject[height][width];
+    }
+
+    public Board(int height, int width, GameObject[] objects, GameCharacter[] characters){
+        this(height, width);
+        this.objects = new GameObject[objects.length];
+        System.arraycopy(objects, 0, this.objects, 0, objects.length);
+        this.characters = new GameCharacter[characters.length];
+        System.arraycopy(characters, 0, this.characters, 0, characters.length);
+    }
 }
