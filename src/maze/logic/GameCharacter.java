6bf3dbae6f09007aae72d5ffe76e0d6acc90f331
AndreBrochado <andrebrreis@gmail.com>
2016-02-27 14:38:23 +0000

    Add GameObject and GameCharacter fields and constructors

--- src/maze/logic/GameCharacter.java
+++ src/maze/logic/GameCharacter.java
@@ -3,5 +3,12 @@ package maze.logic;
 /**
  * Created by Andre on 27/02/2016.
  */
-public class GameCharacter {
+public class GameCharacter extends GameObject {
+
+    final int ARMED = 2;
+
+    public GameCharacter(int x, int y, char[] representations){
+        super(x, y, representations, true, false);
+    }
+
 }
--- src/maze/logic/GameObject.java
+++ src/maze/logic/GameObject.java
@@ -4,4 +4,24 @@ package maze.logic;
  * Created by Andre on 27/02/2016.
  */
 public class GameObject {
+
+    final int VISIBLE = 0, INVISIBLE = 1;
+
+    private int x, y;
+    private int state = 0;
+    private boolean impassable = false, equipable = false;
+    private char[] representations;
+
+    public GameObject(int x, int y, char[] representations){
+        this.x = x;
+        this.y = y;
+        this.representations = new char[representations.length];
+        System.arraycopy(representations, 0, this.representations, 0, representations.length);
+    }
+
+    public GameObject(int x, int y, char[] representations, boolean impassable, boolean equipable){
+        this(x, y, representations);
+        this.impassable = impassable;
+        this.equipable = equipable;
+    }
 }
