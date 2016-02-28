5f8426e81af2444598ffb4c0a4808b4491b41f63
Vasco Ribeiro <vascovsc1996@gmail.com>
2016-02-28 16:00:42 +0000

    Implementation of function input_reader

--- src/maze/cli/MazeGame.java
+++ src/maze/cli/MazeGame.java
@@ -1,7 +1,18 @@
 package maze.cli;
+import java.util.Scanner;
 
 /**
  * Created by Andre on 27/02/2016.
  */
+
+
+
 public class MazeGame {
+
+public char input_reader(){
+    Scanner reader = new Scanner(System.in);
+    char c = reader.next().charAt(0);
+    return c;
+}
+
 }
