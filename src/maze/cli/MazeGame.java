3220b4088f5c760b5aae558c6d5625554c6c6cfe
Vasco Ribeiro <vascovsc1996@gmail.com>
2016-02-28 16:38:37 +0000

    First and second menu implementation

--- src/maze/cli/MazeGame.java
+++ src/maze/cli/MazeGame.java
@@ -15,4 +15,61 @@ public char input_reader(){
     return c;
 }
 
+    public void NewGameType(){
+
+        int ch;
+        Scanner sc=new Scanner(System.in);
+
+        while(true)
+        {
+            System.out.println("1. Immobile Dragon");
+            System.out.println("2. Dragon with random movement");
+            System.out.println("3. Dragon with random movement and rest");
+            System.out.println("4. Exit");
+            System.out.print("Insert your choice: ");
+            ch = sc.nextInt();
+            switch(ch)
+            {
+                case 1:
+                    Game1();
+                    break;
+                case 2:
+                    Game2();
+                    break;
+                case 3:
+                    Game3();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("invalid");
+            }
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        int ch;
+        Scanner sc=new Scanner(System.in);
+
+        while(true)
+        {
+            System.out.println("1. New Game");
+            System.out.println("2. Exit");
+            System.out.print("Insert your choice: ");
+            ch = sc.nextInt();
+            switch(ch)
+            {
+                case 1:
+                    NewGameType();
+                    break;
+                case 2:
+                    System.exit(0);
+                default:
+                    System.out.println("invalid");
+            }
+        }
+    }
+
 }
