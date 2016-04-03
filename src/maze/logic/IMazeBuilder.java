fdd0b8f1c27b23088b81956ac19a6f0ce1575487
Vasco Ribeiro <vascovsc1996@gmail.com>
2016-04-03 23:35:03 +0100

    Code Documentation finish

--- src/maze/logic/IMazeBuilder.java
+++ src/maze/logic/IMazeBuilder.java
@@ -1,8 +1,5 @@
 package maze.logic;
 
-/**
- * Created by Andre on 08/03/2016.
- */
 
 public interface IMazeBuilder {
     public GameObject[][] buildMaze(int size) throws IllegalArgumentException;
